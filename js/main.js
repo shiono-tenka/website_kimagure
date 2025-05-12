af25197aa44f69285c1b24bb3e8a846d128c722e
shiono-tenka <187812990+shiono-tenka@users.noreply.github.com>
2025-05-13 08:25:45 +0900

    追加：detailページ

--- js/main.js
+++ js/main.js
@@ -11,18 +11,23 @@
 //         });
 // });
 
+// 画面読み込んだ時に動く＝DOMContentLoaded
+// DOMContentLoadedの中でaddEventListener（今回はクリック）のときに、を定義
+// 変数を定義してるのが18行〜19行、document.querySelectorはhtmlの要素を指定してる（何を=hamburger、どこが=nav、の部分）
 document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.querySelector('.hamburger');
     const nav = document.querySelector('.header__hamburger-content');
-    const menuItems = document.querySelectorAll('.header__hamburger-content--menu li');
+    const menuItems = document.querySelectorAll('.header__hamburger-content--menu li'); 
 
     // ハンバーガーボタンのクリックで開閉
+
     hamburger.addEventListener('click', function () {
         this.classList.toggle('active');
         nav.classList.toggle('active');
     });
 
     // メニューのリンククリックでメニューを閉じる
+    // forEach ループ処理　で検索
     menuItems.forEach(item => {
         item.addEventListener('click', () => {
             hamburger.classList.remove('active');
